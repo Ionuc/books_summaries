9208fd68132aaba714c1c7481ae68d875f5d1871
Ionut Mesaros <ionute.mesaros@yahoo.com>
2016-08-11 22:51:27 +0300

    Add Fibonacci
    - with different implementations for generating the n'th element
    - add UTs

---
+++ summaries/CrackingTheTechnicalInterviewEbook/src/test/groovy/com/imesaros/crackingthetechnicalinterview/chapter1/FibonacciSpecs.groovy
@@ -0,0 +1,47 @@
+package com.imesaros.crackingthetechnicalinterview.chapter1
+
+import spock.lang.Specification
+
+class FibonacciSpecs extends Specification {
+
+    Fibonacci algorithm = new Fibonacci()
+
+    def 'should generate the n\'th element with formula 1'() {
+        expect:
+        algorithm.generate1(n) == expected
+
+        where:
+        n  | expected
+        -2 | -1
+        0  | 0
+        1  | 1
+        5  | 5
+        10 | 55
+    }
+
+    def 'should generate the n\'th element with formula 2'() {
+        expect:
+        algorithm.generate2(n) == expected
+
+        where:
+        n  | expected
+        -2 | -1
+        0  | 0
+        1  | 1
+        5  | 5
+        10 | 55
+    }
+
+    def 'should generate the n\'th element with formula 3'() {
+        expect:
+        algorithm.generate3(n) == expected
+
+        where:
+        n  | expected
+        -2 | -1
+        0  | 0
+        1  | 1
+        5  | 5
+        10 | 55
+    }
+}
