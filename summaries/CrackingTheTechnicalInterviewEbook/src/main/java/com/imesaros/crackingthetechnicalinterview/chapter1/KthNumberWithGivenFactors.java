98af88224dd02b6cf3d3dd0232671b0bb649ceb4
Ionut Mesaros <ionute.mesaros@yahoo.com>
2016-08-13 01:14:38 +0300

    Refactor KthNumberWithGivenFactors
    - to use Consumers

--- summaries/CrackingTheTechnicalInterviewEbook/src/main/java/com/imesaros/crackingthetechnicalinterview/chapter1/KthNumberWithGivenFactors.java
+++ summaries/CrackingTheTechnicalInterviewEbook/src/main/java/com/imesaros/crackingthetechnicalinterview/chapter1/KthNumberWithGivenFactors.java
@@ -3,6 +3,7 @@ package com.imesaros.crackingthetechnicalinterview.chapter1;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.function.Consumer;
 
 import static java.util.Arrays.asList;
 
@@ -14,6 +15,26 @@ public class KthNumberWithGivenFactors {
     private List<Integer> factorsOf3 = new ArrayList<>(asList(3));
     private List<Integer> factorsOf5 = new ArrayList<>(asList(5));
     private List<Integer> factorsOf7 = new ArrayList<>(asList(7));
+
+    private Consumer<Integer> consumeFromFactorsOf7 = (Integer min) -> {
+        factorsOf7.add(7 * min);
+        Collections.sort(factorsOf7);
+    };
+
+    private Consumer<Integer> consumeFromFactorsOf5 = (Integer min) -> {
+        factorsOf5.add(5 * min);
+        Collections.sort(factorsOf5);
+
+        consumeFromFactorsOf7.accept(min);
+    };
+
+    private Consumer<Integer> consumeFromFactorsOf3 = (Integer min) -> {
+        factorsOf3.add(3 * min);
+        Collections.sort(factorsOf3);
+
+        consumeFromFactorsOf5.accept(min);
+    };
+
 
     public int findKthElement(int k) {
         if (k <= 0) {
@@ -28,21 +49,16 @@ public class KthNumberWithGivenFactors {
 
             if (factorsOf3.contains(min)) {
                 factorsOf3.remove(0);
-                factorsOf3.add(3 * min);
-                Collections.sort(factorsOf3);
+                consumeFromFactorsOf3.accept(min);
             }
             if (factorsOf5.contains(min)) {
                 factorsOf5.remove(0);
+                consumeFromFactorsOf5.accept(min);
             }
             if (factorsOf7.contains(min)) {
                 factorsOf7.remove(0);
+                consumeFromFactorsOf7.accept(min);
             }
-            factorsOf5.add(5 * min);
-            Collections.sort(factorsOf5);
-
-            factorsOf7.add(7 * min);
-            Collections.sort(factorsOf5);
-
             count++;
         }
         return min;
