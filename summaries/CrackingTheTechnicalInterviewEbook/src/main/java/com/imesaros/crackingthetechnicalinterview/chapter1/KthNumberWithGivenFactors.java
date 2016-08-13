d280762b97bee7411e5957524db2c0a6dbbb36a0
Ionut Mesaros <ionute.mesaros@yahoo.com>
2016-08-13 15:30:59 +0300

    Generalize KthNumberWithGivenFactors
    - to generate k'th element such that only prime factors are given
    inside method parameter

--- summaries/CrackingTheTechnicalInterviewEbook/src/main/java/com/imesaros/crackingthetechnicalinterview/chapter1/KthNumberWithGivenFactors.java
+++ summaries/CrackingTheTechnicalInterviewEbook/src/main/java/com/imesaros/crackingthetechnicalinterview/chapter1/KthNumberWithGivenFactors.java
@@ -1,66 +1,76 @@
 package com.imesaros.crackingthetechnicalinterview.chapter1;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 import java.util.function.Consumer;
 
 import static java.util.Arrays.asList;
+import static java.util.function.Function.identity;
+import static java.util.stream.Collectors.toList;
+import static java.util.stream.Collectors.toMap;
 
 /**
  * Problem : 8 Design an algorithm to find the kth number such that the only prime factors are 3, 5, and 7
+ * The implementations is generalized for prime numbers p1, p2 , .. pn given inside list of primeFactors
  */
 public class KthNumberWithGivenFactors {
 
-    private List<Integer> factorsOf3 = new ArrayList<>(asList(3));
-    private List<Integer> factorsOf5 = new ArrayList<>(asList(5));
-    private List<Integer> factorsOf7 = new ArrayList<>(asList(7));
+    public int findKthElementWithFactors3And5And7(int k) {
+        return findKthElementWithFactors(k, asList(3, 5, 7));
+    }
 
-    private Consumer<Integer> consumeFromFactorsOf7 = (Integer min) -> {
-        factorsOf7.add(7 * min);
-        Collections.sort(factorsOf7);
-    };
-
-    private Consumer<Integer> consumeFromFactorsOf5 = (Integer min) -> {
-        factorsOf5.add(5 * min);
-        Collections.sort(factorsOf5);
-
-        consumeFromFactorsOf7.accept(min);
-    };
-
-    private Consumer<Integer> consumeFromFactorsOf3 = (Integer min) -> {
-        factorsOf3.add(3 * min);
-        Collections.sort(factorsOf3);
-
-        consumeFromFactorsOf5.accept(min);
-    };
-
-
-    public int findKthElement(int k) {
+    public int findKthElementWithFactors(int k, List<Integer> primeFactors) {
         if (k <= 0) {
             return 0;
+        }
+        List<Integer> primeOrderFactors = primeFactors.stream().sorted().collect(toList());
+
+        Map<Integer, List<Integer>> factorsOf = primeOrderFactors.stream().collect(toMap(
+                identity(), value -> new ArrayList<Integer>(asList(value))
+        ));
+
+        Map<Integer, Consumer<Integer>> consumers = new HashMap<>();
+        for (int i = primeOrderFactors.size() - 1; i >= 0; i--) {
+            if (i == primeOrderFactors.size() - 1) {
+                consumers.put(primeOrderFactors.get(i), new CustomConsumer(factorsOf.get(primeOrderFactors.get(i)), primeOrderFactors.get(i), value -> {
+                }));
+            } else {
+                consumers.put(primeOrderFactors.get(i), new CustomConsumer(factorsOf.get(primeOrderFactors.get(i)), primeOrderFactors.get(i), consumers.get(primeOrderFactors.get(i + 1))));
+            }
         }
 
         int count = 0;
         int min = 0;
         while (count < k) {
-            min = Integer.min(factorsOf3.get(0), factorsOf5.get(0));
-            min = Integer.min(min, factorsOf7.get(0));
-
-            if (factorsOf3.contains(min)) {
-                factorsOf3.remove(0);
-                consumeFromFactorsOf3.accept(min);
-            }
-            if (factorsOf5.contains(min)) {
-                factorsOf5.remove(0);
-                consumeFromFactorsOf5.accept(min);
-            }
-            if (factorsOf7.contains(min)) {
-                factorsOf7.remove(0);
-                consumeFromFactorsOf7.accept(min);
-            }
+            Map.Entry<Integer, Integer> pair = factorsOf.entrySet().stream()
+                    .map(entry -> new AbstractMap.SimpleImmutableEntry<Integer, Integer>(entry.getKey(), entry.getValue().get(0)))
+                    .min((o1, o2) -> o1.getValue().compareTo(o2.getValue())).get();
+            min = pair.getValue();
+            factorsOf.get(pair.getKey()).remove(0);
+            consumers.get(pair.getKey()).accept(min);
             count++;
         }
         return min;
+
+    }
+
+    private class CustomConsumer implements Consumer<Integer> {
+
+        private final List<Integer> factorsOf;
+        private final Integer multiplier;
+        private final Consumer<Integer> nextConsumer;
+
+        private CustomConsumer(List<Integer> factorsOf, Integer multiplier, Consumer<Integer> nextConsumer) {
+            this.factorsOf = factorsOf;
+            this.multiplier = multiplier;
+            this.nextConsumer = nextConsumer;
+        }
+
+        @Override
+        public void accept(Integer value) {
+            factorsOf.add(multiplier * value);
+            Collections.sort(factorsOf);
+
+            nextConsumer.accept(value);
+        }
     }
 }
--- summaries/CrackingTheTechnicalInterviewEbook/src/test/groovy/com/imesaros/crackingthetechnicalinterview/chapter1/KthNumberWithGivenFactorsSpecs.groovy
+++ summaries/CrackingTheTechnicalInterviewEbook/src/test/groovy/com/imesaros/crackingthetechnicalinterview/chapter1/KthNumberWithGivenFactorsSpecs.groovy
@@ -2,13 +2,15 @@ package com.imesaros.crackingthetechnicalinterview.chapter1
 
 import spock.lang.Specification
 
+import static java.util.Arrays.asList
+
 class KthNumberWithGivenFactorsSpecs extends Specification {
 
     KthNumberWithGivenFactors algorithm = new KthNumberWithGivenFactors()
 
-    def 'should generate kt\'th element'() {
+    def 'should generate kt\'th element using factors of 3, 5 and 7'() {
         expect:
-        algorithm.findKthElement(k) == expected
+        algorithm.findKthElementWithFactors3And5And7(k) == expected
 
         where:
         k  | expected
@@ -24,5 +26,25 @@ class KthNumberWithGivenFactorsSpecs extends Specification {
         10 | 45
         11 | 49
         12 | 63
+    }
+
+    def 'should generate kt\'th element using factors of given prime factors'() {
+        expect:
+        algorithm.findKthElementWithFactors(k, primes) == expected
+
+        where:
+        k  | primes           | expected
+        1  | asList(5, 7, 11) | 5
+        2  | asList(5, 7, 11) | 7
+        3  | asList(5, 7, 11) | 11
+        4  | asList(5, 7, 11) | 25
+        5  | asList(5, 7, 11) | 35
+        6  | asList(5, 7, 11) | 49
+        7  | asList(5, 7, 11) | 55
+        8  | asList(5, 7, 11) | 77
+        9  | asList(5, 7, 11) | 121
+        10 | asList(5, 7, 11) | 125
+        11 | asList(5, 7, 11) | 175
+        12 | asList(5, 7, 11) | 245
     }
 }
