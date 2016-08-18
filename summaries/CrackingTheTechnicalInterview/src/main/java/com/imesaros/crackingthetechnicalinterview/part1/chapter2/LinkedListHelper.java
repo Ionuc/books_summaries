d009718d25e9b8594a747d4fa102fd568045ad2d
Ionut Mesaros <ionut.mesaros@ullink.com>
2016-08-18 11:03:25 +0300

    Add findFirstLoop() to LinkedListHelper
    - which is finding the first node which is creating a cycle inside
    the circular linked list
    - ad UTs

--- summaries/CrackingTheTechnicalInterview/src/main/java/com/imesaros/crackingthetechnicalinterview/part1/chapter2/LinkedListHelper.java
+++ summaries/CrackingTheTechnicalInterview/src/main/java/com/imesaros/crackingthetechnicalinterview/part1/chapter2/LinkedListHelper.java
@@ -5,6 +5,13 @@ import java.util.Map;
 
 public class LinkedListHelper
 {
+    /**
+     * Write code to remove duplicates from an unsorted linked list.
+     * FOLLOW UP
+     * How would you solve this problem if a temporary buffer is not allowed?
+     *
+     * @param head
+     */
     public static void removeDuplicatesWithBuffer(LinkedListNode head)
     {
         Map<Integer, Boolean> table = new HashMap<>();
@@ -28,7 +35,7 @@ public class LinkedListHelper
 
     public static void removeDuplicatesWithoutBuffer(LinkedListNode head)
     {
-        if (head == null | head.getNext() == null)
+        if (head == null || head.getNext() == null)
         {
             return;
         }
@@ -53,6 +60,18 @@ public class LinkedListHelper
         }
     }
 
+    /**
+     * You have two numbers represented by a linked list, where each node contains a single digit.
+     * The digits are stored in reverse order, such that the 1’s digit is at the head of the list.
+     * Write a function that adds the two numbers and returns the sum as a linked list.
+     * EXAMPLE
+     * Input: (3 -> 1 -> 5) + (5 -> 9 -> 2)
+     * Output: 8 -> 0 -> 8
+     *
+     * @param root1
+     * @param root2
+     * @return
+     */
     public static LinkedListNode add(LinkedListNode root1, LinkedListNode root2)
     {
         if (root1 == null)
@@ -111,5 +130,45 @@ public class LinkedListHelper
             LinkedListNode node = new LinkedListNode(carry);
             root.setNext(node);
         }
+    }
+
+    /**
+     * Given a circular linked list, implement an algorithm which returns node at the beginning of the loop.
+     * DEFINITION
+     * Circular linked list: A (corrupt) linked list in which a node’s next pointer points to an
+     * earlier node, so as to make a loop in the linked list.
+     * EXAMPLE
+     * input: A -> B -> C -> D -> E -> C [the same C as earlier]
+     * output: C
+     *
+     * @param root
+     * @return
+     */
+    public static LinkedListNode findFirstLoop(LinkedListNode root)
+    {
+        if (root == null || root.getNext() == null)
+        {
+            return null;
+        }
+
+        LinkedListNode node = root.getNext();
+        int currentPosition = 1;
+        while (node != null)
+        {
+            int foundedPosition = 0;
+            LinkedListNode found = root;
+            while (found != null && found != node)
+            {
+                found = found.getNext();
+                foundedPosition++;
+            }
+            if (currentPosition != foundedPosition)
+            {
+                return node;
+            }
+            node = node.getNext();
+            currentPosition++;
+        }
+        return null;
     }
 }
--- summaries/CrackingTheTechnicalInterview/src/test/groovy/com/imesaros/crackingthetechnicalinterview/part1/chapter2/LinkedListHelperSpecs.groovy
+++ summaries/CrackingTheTechnicalInterview/src/test/groovy/com/imesaros/crackingthetechnicalinterview/part1/chapter2/LinkedListHelperSpecs.groovy
@@ -59,6 +59,43 @@ class LinkedListHelperSpecs extends Specification {
         createLinkedList([3, 1, 5]) | createLinkedList([5, 9, 4, 1, 1, 1]) | createLinkedList([8, 0, 0, 2, 1, 1])
     }
 
+
+    static LinkedListNode NODE1 = createNode(1)
+    static LinkedListNode NODE2 = createNode(2)
+    static LinkedListNode NODE3 = createNode(3)
+    static LinkedListNode NODE4 = createNode(4)
+    static LinkedListNode NODE5 = createNode(5)
+    static LinkedListNode NODE6 = createNode(6)
+    static LinkedListNode NODE7 = createNode(7)
+
+
+    def 'should find first node which is creating a cycle'() {
+        when:
+        LinkedListNode result = findFirstLoop(root)
+
+        then:
+        result == expected
+
+        where:
+        root                      | expected
+        link(NODE1, NODE2, NODE1) | NODE1
+        link(NODE3, NODE4, NODE4) | NODE4
+        link(NODE5, NODE6)        | null
+        link(NODE7)               | null
+        null                      | null
+    }
+
+    private static LinkedListNode link(LinkedListNode[] nodes) {
+        for (int i = 0; i < nodes.length - 1; i++) {
+            nodes[i].setNext(nodes[i + 1])
+        }
+        return nodes[0]
+    }
+
+    private static LinkedListNode createNode(int value) {
+        return new LinkedListNode(value)
+    }
+
     private static LinkedListNode createLinkedList(List<Integer> integers) {
         LinkedListNode head = new LinkedListNode(integers.get(0))
         LinkedListNode list = head
