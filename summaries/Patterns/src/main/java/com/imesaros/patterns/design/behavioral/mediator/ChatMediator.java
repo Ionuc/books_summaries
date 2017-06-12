4a8fa255a4cb7c25fcae99be3063e8d3fb7abfcd
Ionut Mesaros <ionut.mesaros@ullink.com>
2017-06-12 16:35:04 +0300

    Add Mediator Behavioral Design Pattern example

---
+++ summaries/Patterns/src/main/java/com/imesaros/patterns/design/behavioral/mediator/ChatMediator.java
@@ -0,0 +1,9 @@
+package com.imesaros.patterns.design.behavioral.mediator;
+
+public interface ChatMediator
+{
+
+    public void sendMessage(String msg, User user);
+
+    void addUser(User user);
+}
